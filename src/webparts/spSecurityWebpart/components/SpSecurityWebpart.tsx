3ad004fc1c03705e33f81283eed85aa8ff225d6d
russellgove@gmail.com <russellgove@gmail.com>
2016-09-12 19:20:27 -0400

    fix react

--- src/webparts/spSecurityWebpart/components/SpSecurityWebpart.tsx
+++ src/webparts/spSecurityWebpart/components/SpSecurityWebpart.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { css } from 'office-ui-fabric-react';
-
+import spSecurityService from "../spsecurityService";
+import {SPSecurityInfo}from "../spsecurityService";
 
 import styles from '../SpSecurityWebpart.module.scss';
 import { ISpSecurityWebpartWebPartProps } from '../ISpSecurityWebpartWebPartProps';
@@ -9,20 +10,29 @@ export interface ISpSecurityWebpartProps extends ISpSecurityWebpartWebPartProps 
 }
 
 export default class SpSecurityWebpart extends React.Component<ISpSecurityWebpartProps, {}> {
-  public componentWillMount(): void{
+  private svc: spSecurityService = new spSecurityService("ss");
+  public componentWillMount(): void {
+    this.svc.loadData(false).then((response) => {
+      debugger;
+      this.setState(response);
+    });
 
   }
+  public getInitialState(): any {
+    return new SPSecurityInfo();
+  }
+
   public render(): JSX.Element {
     return (
       <div className={styles.spSecurityWebpart}>
         <div className={styles.container}>
-          <div className={css('ms-Grid-row ms-bgColor-themeDark ms-fontColor-white', styles.row)}>
+          <div className={css('ms-Grid-row ms-bgColor-themeDark ms-fontColor-white', styles.row) }>
             <div className='ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1'>
               <span className='ms-font-xl ms-fontColor-white'>
                 Welcome to SharePoint!
               </span>
-               <p className='ms-font-l ms-fontColor-white'>
-              siteuserds {this.props.users.length}
+              <p className='ms-font-l ms-fontColor-white'>
+                siteuserds {this.props.users.length}
 
               </p>
               <p className='ms-font-l ms-fontColor-white'>
@@ -32,9 +42,9 @@ export default class SpSecurityWebpart extends React.Component<ISpSecurityWebpar
                 {this.props.description}
               </p>
               <a
-                className={css('ms-Button', styles.button)}
+                className={css('ms-Button', styles.button) }
                 href='https://github.com/SharePoint/sp-dev-docs/wiki'
-              >
+                >
                 <span className='ms-Button-label'>Learn more</span>
               </a>
             </div>
